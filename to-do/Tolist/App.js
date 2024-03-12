17261d112ea066ec5cd6460b7f2f52ca815b68d9
Shreyas Siddaraju <Shreysid2352@gmail.com>
2024-03-12 19:59:54 +0530

    delete task

--- to-do/Tolist/App.js
+++ to-do/Tolist/App.js
@@ -1,38 +1,80 @@
+import { React, useState } from "react";
 import Card from "./components/Card";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Styles from "./Styles";
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
 
+
+// export const [task, setTask] = useState();
+// export const [taskItems, setTaskItems] = useState([]);
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  // function to append a text to the array
+  const addText = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+    console.log(task);
+  };
+
+  // function to delete a task
+  const deletetask = (index) => {
+    let indexCopy = [...taskItems];
+    indexCopy.splice(index, 1);
+    setTaskItems(indexCopy);
+  };
+
   return (
-    <View style={styles.container}>
+    <View style={Styles.container}>
       {/* This is where the heading appears */}
-      <View style={styles.heading}>
-        <Text style={styles.heading}>Today's Task</Text>
+      <View style={Styles.heading}>
+        <Text style={Styles.heading}>Today's Task</Text>
       </View>
+
       {/* This is where the tasks appear */}
-      <View style={styles.tasks}>
-        <Card Text={'This is a text1'}/>
-        <Card Text={'This is a text2'}/>
-        <Card Text={'This is a text3'}/>
-        <Card Text={'This is a text4'}/>
-        
+      <View style={Styles.tasks}>
+       
+        {
+        taskItems.map((item, index) => {
+          return(
+            <TouchableOpacity onPress={() => deletetask(index)}>
+              <Card Text={item} key={index} />
+            </TouchableOpacity>
+          )
+        })
+        }
       </View>
+      {/* This is where you write the tasks */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={Styles.wrapper}
+      >
+        <TextInput
+          placeholder={"write a task"}
+          style={Styles.input}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+
+        {/* Add button */}
+        <TouchableOpacity activeOpacity={0.5} onPress={() => addText()}>
+          <View style={Styles.add}>
+            <Text style={Styles.addButton}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#E8EAED",
-  },
-  heading: {
-    paddingTop: 20,
-    paddingHorizontal: 10,
-    paddingBottom:3,
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  tasks: {
-    marginTop:30,
-  },
-});
