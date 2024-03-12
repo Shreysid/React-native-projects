0532c1dff979d201f07c17555b21e84ae95b5537
Shreyas Siddaraju <Shreysid2352@gmail.com>
2024-03-12 20:01:35 +0530

    Update Card.jsx

--- to-do/Tolist/components/Card.jsx
+++ to-do/Tolist/components/Card.jsx
@@ -1,43 +1,22 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import Styles from "../Styles";
+
 
 export default function Card(props) {
   return (
-    <View style={styles.cardStyles}>
-      <View>
-        <Text style={styles.Text}>{props.Text}</Text>
+    <View style={Styles.cardStyles}>
+      <View style={Styles.itemLeft}>
+        <TouchableOpacity activeOpacity={0.7}>
+          <View style={Styles.Circle}></View>
+        </TouchableOpacity>
+        <Text style={Styles.Text}>{props.Text}</Text>
       </View>
-
-      <View style={styles.Circle}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  cardStyles: {
-    paddingHorizontal: 20,
-    padding: 15,
-    fontSize: 18,
-    backgroundColor: "#FFF",
-    borderRadius: 10,
-    margin: 5,
-    justifyContent: "space-between",
-  },
-  Text: {
-    maxWidth: "80%",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    // alignItems:'center',
-  },
-  Circle: {
-    width: 12,
-    height: 12,
-    borderColor: "blue",
-    borderWidth: 2,
-    borderRadius: 10,
-    // paddingRight: 10,
-    marginBottom: 10,
-    // flexWrap: "wrap",
-    alignItems: "end",
-  },
-});
+const clicked = () => {
+  console.log("clicked");
+};
+
